76f1c666b268322fb1254bdc560a0a6c9958686c
Sekseason <sekseason@gmail.com>
2019-03-05 02:36:19 +0700

    init swagger

---
+++ Options/SwaggerOptions.cs
@@ -0,0 +1,8 @@
+public class SwaggerOptions
+{
+  public string JsonRoute { get; set; }
+
+  public string Description { get; set; }
+
+  public string UiEndpoint { get; set; }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,39 +10,56 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace dotnet_core_api
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      services.AddSwaggerGen(x =>
+      {
+        x.SwaggerDoc("v1", new Info { Title = "Demo API", Version = "v1" });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      var swaggerOptions = new SwaggerOptions();
+      Configuration.GetSection(nameof(swaggerOptions)).Bind(swaggerOptions);
+
+      app.UseSwagger(option =>
+      {
+        option.RouteTemplate = swaggerOptions.JsonRoute;
+      });
+      app.UseSwaggerUI(option =>
+      {
+        option.SwaggerEndpoint(swaggerOptions.UiEndpoint, swaggerOptions.Description);
+      });
+
+      app.UseHttpsRedirection();
+      app.UseMvc();
+    }
+  }
 }
